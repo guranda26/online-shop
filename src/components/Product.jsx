dcbb85f3110dba00e53dc0b1e45e18427c379eab
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-09-26 00:54:33 +0400

    create add to cart button

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/Product.css";
 
 const Product = ({ src, heading, description }) => {
   return (
@@ -6,6 +7,7 @@ const Product = ({ src, heading, description }) => {
       <h2>{heading}</h2>
       <img src={src} alt={heading} width="200px" />
       <p>{description}</p>
+      <input type="button" value="Add to cart" className="cart-btn" />
     </div>
   );
 };
