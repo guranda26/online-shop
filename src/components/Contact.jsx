5c5282fbfa6ca96d200428611bafcaf7ba761fea
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-09-30 00:55:44 +0400

    create form element

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,11 +5,24 @@ const Contact = () => {
   return (
     <section id="contact" className="section contact">
       <h2>Contact Us</h2>
-      <p>
-        We'd love to hear from you! Whether you have a question about our
-        products, services, or anything else, our team is ready to answer all
-        your questions.
-      </p>
+      <p>We'd love to hear from you! Don't hesitate to ask a question.</p>
+      <p>Our team is ready to answer all your questions.</p>
+      <form action="#" className="form">
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="text" />
+
+        <label htmlFor="surname">Surname:</label>
+        <input id="surname" type="text" />
+
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" />
+
+        <label htmlFor="message">Message:</label>
+        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+
+        <input type="submit" value="Submit" />
+      </form>
+
       <ul>
         <li>
           <span>Email:</span> contact@mail.com
