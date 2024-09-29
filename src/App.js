46bc6fb68733a2fd3347657f4f9268a6bd642533
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-09-29 22:54:46 +0400

    create Layout

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
+import Layout from "./Layout";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<MainContent />} />
-        <Route path="/about" element={<About />} />
-        {/* <Route path="/products" element={<Products />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainContent />} />
+          {/* <Route path="/home" element={<MainContent />} /> */}
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/products" element={<Products />} />
         <Route path="/contact" element={<Contact />} /> */}
+        </Route>
       </Routes>
-      <Header />
-      <MainContent />
+      {/* <Header /> */}
+      {/* <MainContent /> */}
       <Footer />
     </div>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="section contact">
+      <h2>Contact Us</h2>
+      <p>
+        We'd love to hear from you! Whether you have a question about our
+        products, services, or anything else, our team is ready to answer all
+        your questions.
+      </p>
+      <ul>
+        <li>
+          <span>Email:</span> contact@mail.com
+        </li>
+        <li>
+          <span>Phone:</span> +995 595 76-39-32
+        </li>
+        <li>
+          <span>Address:</span> Abashidze str, 32, Tbilisi, Georgia
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Contact;
