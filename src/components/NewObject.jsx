7991bc17089a65d0889361f03197c897700c61b0
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-01 20:18:03 +0400

    remove comments

--- src/components/NewObject.jsx
+++ src/components/NewObject.jsx
@@ -1,18 +1,5 @@
 import "../styles/ObjectTable.css";
 import { obj } from "../utils/object";
-// const obj = {
-//   id: "10002",
-
-//   name: "Eco-Friendly Water Bottle",
-
-//   description: "Stay hydrated with our durable, eco-friendly water bottle.",
-
-//   price: 14.99,
-
-//   currency: "USD",
-
-//   imageURL: "https://example.com/images/product-10002.jpg",
-// };
 
 const parsedObj = obj();
 
