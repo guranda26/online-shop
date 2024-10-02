35bd2420972baad756bd603b3c5b2f9d01ad2bb2
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-02 16:43:59 +0400

    add image in usersData

--- src/utils/usersData.js
+++ src/utils/usersData.js
@@ -1,12 +1,14 @@
 export function usersData() {
   const usersData = {
+    profileImage:
+      "https://assets.vogue.in/photos/5f23c04f1d33754d11eaf778/2:3/w_2240,c_limit/harry-potter-philosophers-stone-portrait-8.jpg",
     firstName: "Harry Potter",
     email: "harrypotter@gmail.com",
     age: 10,
     gender: "Male",
     phone: "+283223344",
     address: "Number 4 Privet Drive, Anytown | Chancellors",
-    hobbies: ["Reading", "Cooking", "Sitting alone"],
+    hobbies: ["Reading", "Defeating Voldemort", "Getting in troubles"],
   };
   return usersData;
 }
