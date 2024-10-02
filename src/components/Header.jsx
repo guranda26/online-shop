438ba3596c85f7bc02b595d42eb11681ccbb18cf
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-02 16:27:05 +0400

    create profile route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
+import { CgProfile } from "react-icons/cg";
 
 const Header = () => {
   return (
     <header className="header">
       <nav className="navbar">
         <ul>
-          <li className="nav-items">
+          <li className="nav-item">
             <Link to={"home"}>Home</Link>
           </li>
-          <li className="nav-items">
+          <li className="nav-item">
             <Link to={"about"}>About</Link>
           </li>
-          <li className="nav-items">
+          <li className="nav-item">
             <Link to={"products"}>Products</Link>
           </li>
-          <li className="nav-items">
+          <li className="nav-item">
             <Link to={"contact"}>Contact</Link>
           </li>
-          <li className="nav-items">
+          <li className="nav-item">
             <Link to={"assignment-3"}>Assignment</Link>
           </li>
-          <li>
+          <li className="nav-item">
             <Link to={"blogs"}>Blogs</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"profile"}>
+              <CgProfile className="profile" />
+            </Link>
           </li>
         </ul>
       </nav>
