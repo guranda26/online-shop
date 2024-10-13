3dfb573e1fb00ff0812091c846803e0807392536
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-13 22:58:09 +0400

    move client side logic of the detailed products page to a new component

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,39 +1,19 @@
-"use client";
-import Link from "next/link";
-import LoadingSpinner from "../../components/Loader";
-import { useFetchItems } from "../../hooks/useFetchItems";
 import "../index.css";
-import NotFoundPage from "../../not-found";
 import ReturnBackButton from "../../components/ReturnBackButton";
+import ProductDetails from "../../components/ProductDetails";
 
 const PostsPage = ({ params }) => {
   const { id } = params;
-  const url = `https://dummyjson.com/products/${id}`;
-  const { items: product, loading, error } = useFetchItems(url, null);
-
-  if (loading) return <LoadingSpinner />;
-
-  if (!product || error) return <NotFoundPage />;
 
   return (
     <section className="products-section product">
       <h1>Product</h1>
-      <div key={product.id} className="products">
-        <Link href={`products/${product.id}`} className="product-list">
-          <div className="image-container">
-            <img src={product.images[0]} alt="" className="product-img" />
-          </div>
-          <div className="product-info">
-            <h2>{product.title}</h2>
-            <p>{product.description}</p>
-            <p className="price">Price: ${product.price}</p>
-          </div>
-        </Link>
-        <div className="return-back">
-          <ReturnBackButton />
-        </div>
+      <ProductDetails id={id} />
+      <div className="return-back">
+        <ReturnBackButton />
       </div>
     </section>
   );
 };
+
 export default PostsPage;
