36cddf83b3c127dfea5cf59479203d4b15ee0bef
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-16 21:29:31 +0400

    add error handling for the posts

--- src/app/components/FetchPosts.jsx
+++ src/app/components/FetchPosts.jsx
@@ -1,8 +1,14 @@
+import NotFoundPage from "../not-found";
+
 export async function fetchPosts(search) {
   const url = `https://dummyjson.com/posts/search?q=${search || ""}`;
   const response = await fetch(url);
+  if (!response.ok) NotFoundPage;
   console.log("url", url);
 
   const data = await response.json();
+  if (!data.posts) {
+    throw new Error("No posts found");
+  }
   return data.posts;
 }
