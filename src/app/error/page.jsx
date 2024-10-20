fad7a56de804208c559347bb356207b2a9d53036
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-21 02:14:35 +0400

    add global error file

---
+++ src/app/error/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import DangerousComponent from "../components/DangerousComponent";
+
+export default function TestErrorPage() {
+  return (
+    <>
+      <h1>Test Error Handling</h1>
+      <DangerousComponent />
+    </>
+  );
+}
