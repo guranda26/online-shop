3e65a38234bc42f2c0c5031c092318c3b55fffbc
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-29 18:29:29 +0400

    add sign in and profile pages

---
+++ src/app/(auth)/layout.jsx
@@ -0,0 +1,22 @@
+export default function AuthLayout({ children }) {
+  return (
+    <>
+      <header className="login-header">
+        <img
+          className="login-header__logo"
+          src="../assets/logo.svg"
+          alt="Logo"
+          style={{ marginLeft: "8rem", marginTop: "1rem" }}
+        />
+      </header>
+
+      <main className="main">{children}</main>
+
+      <footer className="login-page-footer">
+        <p style={{ textAlign: "center" }} className="login-page-footer__txt">
+          Georgia, Copyright &copy; 2024
+        </p>
+      </footer>
+    </>
+  );
+}
--- src/app/(dashboard)/profile/page.jsx
+++ src/app/(dashboard)/profile/page.jsx
@@ -1,18 +1,59 @@
+"use client";
 import React from "react";
-import "../../../styles/ProfilePage.css";
-import { usersData } from "../../../utils/usersData";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import LoadingSpinner from "../../components/Loader";
 
-const user = usersData();
+import "../../../styles/ProfilePage.css";
+// import { usersData } from "../../../utils/usersData";
+
+// const user = usersData();
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+
+    async function fetchUser() {
+      try {
+        const res = await fetch("https://dummyjson.com/user/me", {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (!res.ok) {
+          console.error("Failed to fetch profile data");
+          return;
+        }
+
+        const data = await res.json();
+
+        // Update the user state
+        setUser(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchUser();
+  }, [router]);
+
+  // Render loading state if user is null
+  if (!user) {
+    return <LoadingSpinner />;
+  }
   return (
     <section className="profile-page">
       <h1 className="profile-title">About User</h1>
       <div className="profile-card">
         <div className="profile-image-container">
           <img
-            src={user.profileImage}
-            alt={`${user.name}`}
+            src={user.image || "../../assets/person.svg"}
+            alt={user.firstName || "User"}
             className="profile-image"
           />
         </div>
@@ -59,17 +100,12 @@ const Profile = () => {
           />
         </div>
         <div className="form-group">
-          <label htmlFor="gender">Hobbies</label>
-          {user.hobbies.map((hobby, index) => (
-            <input
-              type="text"
-              id={`hobbies-${index}`}
-              value={hobby}
-              readOnly
-              className="form-input"
-              key={index}
-            />
-          ))}
+          <label htmlFor="address">Address</label>
+          <p className="form-input">
+            <span>{user.address.address}</span>
+            <span>{user.address.city}</span>
+            <span>{user.address.country}</span>
+          </p>
         </div>
 
         <div className="form-group">
@@ -78,16 +114,6 @@ const Profile = () => {
             type="tel"
             id="phone"
             value={user.phone}
-            readOnly
-            className="form-input"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="address">Address</label>
-          <input
-            type="text"
-            id="address"
-            value={user.address}
             readOnly
             className="form-input"
           />
