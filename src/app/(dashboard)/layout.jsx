78df7cfcffd520edc48e77fb52cf4c954e7372d0
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-10-29 19:39:02 +0400

    add layout for the dashboard

---
+++ src/app/(dashboard)/layout.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+import "../../index.css";
+import { useAuthCheck } from "../hooks/useAuthCheck.js";
+import LoadingSpinner from "../components/Loader.jsx";
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+
+export default function DashboardLayout({ children }) {
+  const isLoading = useAuthCheck();
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  return (
+    <>
+      <Header />
+      <main className="main">{children}</main>
+      <Footer />
+    </>
+  );
+}
