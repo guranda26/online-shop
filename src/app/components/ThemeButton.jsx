fb4b0126055510b7bc99ce84b51bd24ffa3e67ab
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-03 00:02:43 +0400

    add system preference for theme

--- src/app/components/ThemeButton.jsx
+++ src/app/components/ThemeButton.jsx
@@ -1,30 +1,52 @@
 "use client";
 
 import { useTheme } from "next-themes";
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaMoon } from "react-icons/fa6";
 import { MdLightMode } from "react-icons/md";
+import { BsLaptop } from "react-icons/bs";
 
 const ThemeButton = () => {
-  const { resolvedTheme, setTheme } = useTheme();
+  const { theme, resolvedTheme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => setMounted(true), []);
 
-  if (!mounted) {
-    return null;
-  }
+  const handleThemeChange = (newTheme) => {
+    setTheme(newTheme);
+    localStorage.setItem("user-theme", newTheme);
+  };
+
+  if (!mounted) return null;
 
   return (
-    <div
-      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-      className="flex items-center justify-center cursor-pointer bg-gray-300 dark:bg-[#0d0114] ml-auto mr-10 h-10 rounded-full transition-colors duration-300 ease-in-out w-[80px]"
-    >
-      {resolvedTheme === "dark" ? (
-        <MdLightMode className="text-white-300 font-bold" />
-      ) : (
-        <FaMoon className="text-gray-800" />
-      )}
+    <div className="flex space-x-4 items-center ml-auto mr-10">
+      <button
+        onClick={() => handleThemeChange("light")}
+        className={`cursor-pointer p-2 rounded-full ${
+          resolvedTheme === "light"
+            ? "bg-gray-300 text-gray-800"
+            : "bg-transparent"
+        }`}
+      >
+        <MdLightMode />
+      </button>
+      <button
+        onClick={() => handleThemeChange("dark")}
+        className={`cursor-pointer p-2 rounded-full ${
+          resolvedTheme === "dark" ? "bg-gray-700 text-white" : "bg-transparent"
+        }`}
+      >
+        <FaMoon />
+      </button>
+      <button
+        onClick={() => handleThemeChange("system")}
+        className={`cursor-pointer p-2 rounded-full ${
+          theme === "system" ? "bg-gray-500 text-white" : "bg-transparent"
+        }`}
+      >
+        <BsLaptop />
+      </button>
     </div>
   );
 };
