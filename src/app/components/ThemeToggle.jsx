da56c8815a177d365b5111ae853c2aa7da39729b
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-02 22:24:17 +0400

    remove unnecessary code

--- src/app/components/ThemeToggle.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-const ThemeToggle = () => {
-  const [darkMode, setDarkMode] = useState(true);
-
-  return <div>Theme toggle</div>;
-};
-
-export default ThemeToggle;
