dffd5d7afae149bcda2d85437873647046fb47e6
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-03 20:43:11 +0400

    update profile to use auth0 information

--- src/app/(dashboard)/profile/page.jsx
+++ src/app/(dashboard)/profile/page.jsx
@@ -5,81 +5,56 @@ import { useRouter } from "next/navigation";
 import LoadingSpinner from "../../components/Loader";
 
 import "../../../styles/ProfilePage.css";
-// import { usersData } from "../../../utils/usersData";
-
-// const user = usersData();
+import { useUser } from "@auth0/nextjs-auth0/client";
+import NotFoundPage from "../../not-found";
 
 const Profile = () => {
-  const [user, setUser] = useState(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem("accessToken");
-
-    async function fetchUser() {
-      try {
-        const res = await fetch("https://dummyjson.com/user/me", {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (!res.ok) {
-          console.error("Failed to fetch profile data");
-          return;
-        }
-
-        const data = await res.json();
-
-        // Update the user state
-        setUser(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    fetchUser();
-  }, [router]);
-
-  // Render loading state if user is null
-  if (!user) {
+  const { user, error, isLoading } = useUser();
+  if (!user || isLoading) {
     return <LoadingSpinner />;
   }
-  return (
-    <section className="profile-page">
-      <h1 className="profile-title">About User</h1>
-      <div className="profile-card">
-        <div className="profile-image-container">
-          <img
-            src={user.image || "../../assets/person.svg"}
-            alt={user.firstName || "User"}
-            className="profile-image"
-          />
-        </div>
+  if (error)
+    return (
+      <div>
+        <NotFoundPage />
       </div>
-      <form className="profile-form">
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            value={user.firstName}
-            readOnly
-            className="form-input"
-          />
+    );
+
+  return (
+    user && (
+      <section className="profile-page">
+        <h1 className="profile-title">About User</h1>
+        <div className="profile-card">
+          <div className="profile-image-container mx-auto">
+            <img
+              src={user.picture || "../../assets/person.svg"}
+              alt={user.name || "User"}
+              className="profile-image translate-x-[50%]"
+            />
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={user.email}
-            readOnly
-            className="form-input"
-          />
-        </div>
-        <div className="form-group">
+        <form className="profile-form">
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              value={user.name}
+              readOnly
+              className="form-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              value={user.email}
+              readOnly
+              className="form-input"
+            />
+          </div>
+          {/* <div className="form-group">
           <label htmlFor="age">Age</label>
           <input
             type="number"
@@ -117,9 +92,10 @@ const Profile = () => {
             readOnly
             className="form-input"
           />
-        </div>
-      </form>
-    </section>
+        </div> */}
+        </form>
+      </section>
+    )
   );
 };
 
