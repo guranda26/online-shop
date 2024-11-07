831e9bc1fe7c00a9a693fc194efe3e2c1dc727e0
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-07 13:01:26 +0400

    remove extra code

--- src/app/(dashboard)/[[...slug]]/client.jsx
+++ src/app/(dashboard)/[[...slug]]/client.jsx
@@ -1,9 +1,6 @@
 "use client";
 
-import dynamic from "next/dynamic";
 import MainContent from "../../components/MainContent";
-
-// const App = dynamic(() => import("../../../App"), { ssr: false });
 
 export function ClientOnly() {
   return <MainContent />;
