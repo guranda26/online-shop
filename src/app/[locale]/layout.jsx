641ec04584cb9cb2557198bc52a59de0d56c7bc3
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-08 03:40:51 +0400

    add translation for the main page

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -2,20 +2,31 @@ import "../../index.css";
 import "../../styles/Header.css";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import Providers from "../components/providers";
+import initTranslations from "../i18n";
+import TranslationsProvider from "../components/TranslationsProvider";
 
 export const metadata = {
   title: "eCommerce app",
   description: "Web site created with Next.js.",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children, params }) {
+  const locale = params?.locale || "en";
+  const { resources } = await initTranslations(locale, ["home"]);
+
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang={locale} suppressHydrationWarning>
       <UserProvider>
         <body>
-          <Providers>
-            <div className="flex-col">{children}</div>
-          </Providers>
+          <TranslationsProvider
+            resources={resources}
+            locale={locale}
+            namespaces={["home", "common", "header", "about-us"]}
+          >
+            <Providers>
+              <div className="flex-col">{children}</div>
+            </Providers>
+          </TranslationsProvider>
         </body>
       </UserProvider>
     </html>
