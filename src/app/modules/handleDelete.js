4e14aec5933be0dd30e445f0a7a0e25c25eeb690
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-25 00:58:31 +0400

    define Error props type

--- src/app/modules/handleDelete.js
+++ src/app/modules/handleDelete.js
@@ -1,4 +1,4 @@
-export const handleDelete = (products, listName, id, setProducts) => {
+export const handleDelete = (products, id, setProducts) => {
   const updatedProducts = products.filter((product) => product.id !== id);
   setProducts(updatedProducts);
 };
