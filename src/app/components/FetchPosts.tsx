3290ecc529a8f2f367e2fe7f78b616e967db4ef3
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-27 01:07:49 +0400

    fetch posts from the supabase

--- src/app/components/FetchPosts.tsx
+++ src/app/components/FetchPosts.tsx
@@ -1,15 +1,41 @@
 import NotFoundPage from "../[locale]/not-found";
 
 export async function fetchPosts(search = "", sortBy = "title", order = "asc") {
-  const url = `https://dummyjson.com/posts/search?q=${search || ""}${sortBy ? `&sortBy=${sortBy}` : ""}${order ? `&order=${order}` : ""}`;
+  const localUrl = "http://localhost:3000/api/posts";
+  const dummyUrl = `https://dummyjson.com/posts/search?q=${search || ""}${sortBy ? `&sortBy=${sortBy}` : ""}${order ? `&order=${order}` : ""}`;
 
-  const response = await fetch(url);
-  if (!response.ok) NotFoundPage;
-  console.log("url", url);
+  try {
+    const localResponse = await fetch(localUrl);
 
-  const data = await response.json();
-  if (!data.posts) {
-    throw new Error("No posts found");
+    if (localResponse.ok) {
+      const localData = await localResponse.json();
+
+      console.log(localData);
+
+      if (localData && localData.length > 0) {
+        return localData;
+      }
+    }
+
+    const dummyResponse = await fetch(dummyUrl);
+
+    if (!dummyResponse.ok) {
+      console.error("Error fetching products:", dummyResponse.statusText);
+      return <NotFoundPage />;
+    }
+
+    const dummyData = await dummyResponse.json();
+    if (!dummyData.products) {
+      throw new Error("No products found");
+    }
+
+    return dummyData.products;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error in fetchProducts:", error.message);
+    } else {
+      console.error("Unknown error in fetchProducts:", error);
+    }
+    return <NotFoundPage />;
   }
-  return data.posts;
 }
