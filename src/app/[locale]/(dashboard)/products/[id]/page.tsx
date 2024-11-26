cce22e4a12ba4f84bd3de0da6e1b61bbbf788263
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-11-27 00:57:22 +0400

    add product fetching with id

--- src/app/[locale]/(dashboard)/products/[id]/page.tsx
+++ src/app/[locale]/(dashboard)/products/[id]/page.tsx
@@ -6,7 +6,7 @@ import "../../../../../index.css";
 const ProductPage = async ({ params }: { params: Params }) => {
   const { id } = params;
 
-  const response = await fetch(`https://dummyjson.com/products/${id}`);
+  const response = await fetch(`http://localhost:3000/api/products/${id}`);
 
   if (!response.ok) {
     return <NotFoundPage />;
@@ -19,17 +19,17 @@ const ProductPage = async ({ params }: { params: Params }) => {
   }
   return (
     <section className="products-section product">
-      <h1>Product</h1>
+      <h1>{product.brand} Product</h1>
       <div key={product.id} className="products">
         <div className="product-list">
           <div className="image-container">
-            <img src={product.images[0]} alt="" className="product-img" />
+            <img src={product.image_link} alt="" className="product-img" />
           </div>
           <div className="product-info">
-            <h2 className="text-blue-800 font-bold text-2xl">
-              {product.title}
-            </h2>
+            <h2 className="text-blue-800 font-bold text-2xl">{product.name}</h2>
             <p>{product.description}</p>
+            <p className="font-bold italic">Brand: {product.brand}</p>
+            <p className="font-semibold">Category: {product.category}</p>
             <p className="price">Price: ${product.price}</p>
           </div>
         </div>
---
+++ src/app/api/products/[id]/route.tsx
@@ -0,0 +1,54 @@
+import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const cookieStore = cookies();
+
+  if (!cookieStore) {
+    return new Response("Cookie store not available", { status: 400 });
+  }
+
+  const productId = params.id; // Access the dynamic route parameter `[id]`
+
+  if (!productId) {
+    return new Response("Product ID is required", { status: 400 });
+  }
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options);
+            });
+          } catch (error) {
+            console.error("Error setting cookies:", error);
+          }
+        },
+      },
+    }
+  );
+
+  const { data, error } = await supabase
+    .from("products")
+    .select()
+    .eq("id", productId)
+    .single();
+
+  if (error) {
+    console.error("Error fetching product:", error);
+    return new Response("Error fetching product", { status: 500 });
+  }
+
+  return new Response(JSON.stringify(data), { status: 200 });
+};
