8b9fb5a898a824a865cde46d77fd4543b4ff2e57
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-12-21 15:28:36 +0400

    install stripe

--- src/app/[locale]/(dashboard)/posts/page.tsx
+++ src/app/[locale]/(dashboard)/posts/page.tsx
@@ -78,51 +78,58 @@ const PostsPage: React.FC<PostsAndProductPageType> = () => {
 
   return (
     <section className="posts-section p-7 bg-postBackground w-screen text-formText">
-      <h1 className="text-center text-3xl font-semibold">All Posts</h1>
-      <div className="search-sort__wrapper">
-        <SearchInput searchPath="posts" supportsBodySort={true} />
-      </div>
-      <div className="search-container">
-        <div className="search-input-wrapper text-textBlack">
-          <input
-            type="text"
-            placeholder="Title"
-            value={newPost.title}
-            onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
-            className="search-input text-black"
-          />
-          <textarea
-            placeholder="Body"
-            value={newPost.body}
-            onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
-            className="search-input"
-          />
-          <button onClick={handleSubmit} className="search-btn">
-            {editingPost ? "Save Changes" : "Add Post"}
-          </button>
+      <div className="max-w-[1400px]">
+        <h1 className="text-center text-3xl font-semibold">All Posts</h1>
+        <div className="search-sort__wrapper">
+          <SearchInput searchPath="posts" supportsBodySort={true} />
         </div>
-      </div>
-
-      <div className="post-list_wrapper">
-        {posts.map((post) => (
-          <div key={post.id} className="posts bg-postFormBg">
-            <Link href={`posts/${post.id}`} className="post-list">
-              <h2>{post[titleKey]}</h2>
-              <p>{post[bodyKey]}</p>
-            </Link>
-            <div className="buttons">
-              <button
-                className="search-btn delete"
-                onClick={() => onDelete(post.id)}
-              >
-                Delete
-              </button>
-              <button className="search-btn edit" onClick={() => onEdit(post)}>
-                Edit
-              </button>
-            </div>
+        <div className="search-container">
+          <div className="search-input-wrapper text-textBlack">
+            <input
+              type="text"
+              placeholder="Title"
+              value={newPost.title}
+              onChange={(e) =>
+                setNewPost({ ...newPost, title: e.target.value })
+              }
+              className="search-input text-black"
+            />
+            <textarea
+              placeholder="Body"
+              value={newPost.body}
+              onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
+              className="search-input"
+            />
+            <button onClick={handleSubmit} className="search-btn">
+              {editingPost ? "Save Changes" : "Add Post"}
+            </button>
           </div>
-        ))}
+        </div>
+
+        <div className="post-list_wrapper">
+          {posts.map((post) => (
+            <div key={post.id} className="posts bg-postFormBg">
+              <Link href={`posts/${post.id}`} className="post-list">
+                <h2>{post[titleKey]}</h2>
+                <p>{post[bodyKey]}</p>
+              </Link>
+              <div className="buttons">
+                <button
+                  className="search-btn delete"
+                  onClick={() => onDelete(post.id)}
+                >
+                  Delete
+                </button>
+                <button
+                  className="search-btn edit"
+                  onClick={() => onEdit(post)}
+                >
+                  Edit
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
