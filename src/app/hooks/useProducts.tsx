de6e72cbe5a0287eee307ed3c99bc80b38518647
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-12-23 22:40:26 +0400

    Revert "Revert "create custom hook for fetching products""
    This reverts commit 46e54649e43013bee11539951f6ecb65683fff63.

---
+++ src/app/hooks/useProducts.tsx
@@ -0,0 +1,29 @@
+// src/hooks/useProducts.tsx
+import { useState, useEffect } from "react";
+import { fetchProducts } from "../components/FetchProducts";
+import { Product } from "../interfaces/products";
+
+export const useProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const loadProducts = async () => {
+    try {
+      const fetchedProducts = await fetchProducts();
+      setProducts(fetchedProducts);
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+      setError("Error loading products");
+    }
+  };
+
+  useEffect(() => {
+    if (products.length === 0 && !error) {
+      loadProducts();
+    }
+  }, []);
+
+  return { products, setProducts, error, setError };
+};
