1f076152b199fae9f2ff9e07f8219e17dbdfd5c4
David Tarkhnishvili <datotarkh@gmail.com>
2024-12-24 23:17:43 +0100

    added add to cart button and functionality

--- src/app/components/AddToCart.tsx
+++ src/app/components/AddToCart.tsx
@@ -1,23 +1,44 @@
 "use server";
 
 import { createClient } from "@/src/utils/supabase/server";
+import Stripe from "stripe";
 
-const AddToCart = (productId: any) => {
+
+interface AddToCartProps {
+  productId: number;
+  productName: string;
+  productPrice: any;
+}
+
+const AddToCart = ({ productId, productName, productPrice }: AddToCartProps) => {
   async function AddProduct(formData: FormData) {
     "use server";
 
     const supabase = await createClient();
     const user = await supabase.auth.getUser();
 
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+    
+    
+    const stripeProduct = await stripe.products.create({
+      name: productName,
+    })
+
+    const stripePrice = await stripe.prices.create({
+      product: stripeProduct.id,
+      unit_amount: Math.round(productPrice * 100), 
+      currency: "usd",
+    })
+    
     const { data, error } = await supabase
-      .from("cart")
-      .insert({
-        product_id: productId.productId,
-        user_id: user.data.user?.id,
-        stripe_product_id: "efr",
-        stripe_price_id: "rgrg",
-      })
-      .single();
+    .from("cart")
+    .insert({
+      product_id: productId,
+      user_id: user.data.user?.id,
+      stripe_product_id: stripeProduct.id,
+      stripe_price_id: stripePrice.id,
+    })
+    .single();
 
     console.log(user.data.user?.id);
     console.log("data", data, "error", error);
