f4382b9dd99c105a4c9f05580f7502fb0d8d88b0
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-12-27 03:17:38 +0400

    mend

--- src/app/[locale]/(dashboard)/cart/page.tsx
+++ src/app/[locale]/(dashboard)/cart/page.tsx
@@ -1,5 +1,6 @@
 import RedirectToProductBtn from "../../../components/RedirectToProductPage";
 import { MdDelete } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
 
 interface CartItem {
   id: number;
@@ -24,79 +25,122 @@ const Page = async () => {
   const cart: CartItem[] = await response.json();
 
   return (
-    <div className="w-[70vw] p-6 mx-auto">
-      <h1 className="text-2xl font-bold text-gray-800 mb-6">My Cart</h1>
+    <div className="w-full md:w-[75vw] p-3 md:p-6 mx-auto">
+      <h1 className="text-xl md:text-2xl font-bold text-gray-800 mb-3 md:mb-6">
+        My Cart
+      </h1>
       {cart.length > 0 ? (
-        <div className="overflow-x-auto">
-          <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
-            <thead className="bg-gray-100">
-              <tr>
-                <th className="px-6 py-3 text-sm font-medium text-gray-700"></th>
-                <th className="px-6 py-3 text-sm font-medium text-gray-700">
-                  Product Name
-                </th>
-                <th className="px-6 py-3 text-sm font-medium text-gray-700">
-                  Quantity
-                </th>
-                <th className="px-6 py-3 text-sm font-medium text-gray-700">
-                  Price per item
-                </th>
-                <th className="px-6 py-3 text-sm font-medium text-gray-700">
-                  Total Price
-                </th>
-                <th className="px-6 py-3 text-sm font-medium text-gray-700"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {cart.map((item) => (
-                <tr key={item.id} className="border-t">
-                  <td className="px-6 py-4">
-                    <img
-                      src={item.products.image_link}
-                      alt={item.products.name}
-                      className="min-w-[20px] max-w-[60px] rounded-lg"
-                    />
-                  </td>
-                  <td className="px-6 py-4 text-gray-700">
+        <>
+          <div className="md:hidden">
+            {cart.map((item) => (
+              <div
+                key={item.id}
+                className="border-t p-2 flex justify-between items-center"
+              >
+                <img
+                  src={item.products.image_link}
+                  alt={item.products.name}
+                  className="min-w-[20px] max-w-[45px] rounded-full mr-2"
+                />
+                <div className="flex-1">
+                  <h2 className="text-sm font-medium text-gray-700">
                     {item.products.name}
-                  </td>
-                  <td className="px-6 py-4">
-                    <form className="flex gap-2 flex-wrap">
-                      <input
-                        type="number"
-                        className="input w-[70px] border border-gray-300 rounded-md p-1"
-                        min="1"
-                        value={1}
-                      />
-                      <button className="px-3 py-1 rounded-md bg-blue-500 text-white focus:outline-none hover:bg-blue-600 transition-colors">
-                        Update
-                      </button>
-                    </form>
-                  </td>
-                  <td className="px-3 py-4 text-gray-700 font-semibold text-center">
-                    {item.products.price}
-                  </td>
-                  <td className="pl-3 py-4 text-gray-700 font-semibold text-center">
-                    {item.products.price}
-                  </td>
-                  <td className="px-1 py-4">
-                    <button className="py-1  text-red-600 transition-colors">
-                      <MdDelete size={20} />
-                    </button>
-                  </td>
+                  </h2>
+                  <p className="text-xs text-gray-600">
+                    Price: ${item.products.price}
+                  </p>
+                </div>
+                <div>
+                  <input
+                    type="number"
+                    className="w-12 p-1 border rounded text-sm"
+                    defaultValue={1}
+                  />
+                  <button className="text-green-500 ml-2">
+                    <FaRegEdit size={14} />
+                  </button>
+                </div>
+                <div>
+                  <button className="text-red-600">
+                    <MdDelete size={20} />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="hidden md:block overflow-x-auto">
+            <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
+              <thead className="bg-gray-100">
+                <tr>
+                  <th className="px-4 py-3 text-md font-medium text-gray-700"></th>
+                  <th className="px-4 py-3 text-md font-medium text-gray-700">
+                    Product Name
+                  </th>
+                  <th className="px-4 py-3 text-md font-medium text-gray-700">
+                    Quantity
+                  </th>
+                  <th className="px-4 py-3 text-md font-medium text-gray-700">
+                    Price per item
+                  </th>
+                  <th className="px-4 py-3 text-md font-medium text-gray-700">
+                    Total Price
+                  </th>
+                  <th className="px-4 py-3 text-md font-medium text-gray-700"></th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {cart.map((item) => (
+                  <tr key={item.id} className="border-t">
+                    <td className="px-3 py-4">
+                      <img
+                        src={item.products.image_link}
+                        alt={item.products.name}
+                        className="min-w-[20px] max-w-[45px] rounded-full"
+                      />
+                    </td>
+                    <td className="px-4 py-4 text-gray-700 text-sm">
+                      {item.products.name}
+                    </td>
+                    <td className="px-4 py-4">
+                      <form className="flex gap-2">
+                        <input
+                          type="number"
+                          className="w-10 border border-gray-300 rounded-md p-1"
+                          min="1"
+                          defaultValue={1}
+                        />
+                        <button className="text-green-500">
+                          <FaRegEdit size={16} />
+                        </button>
+                      </form>
+                    </td>
+                    <td className="px-4 py-4 text-gray-700 text-center font-semibold">
+                      ${item.products.price}
+                    </td>
+                    <td className="px-4 py-4 text-gray-700 text-center font-semibold">
+                      ${item.products.price}
+                    </td>
+                    <td className="py-4 text-center">
+                      <button className="text-red-600">
+                        <MdDelete size={20} />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </>
       ) : (
-        <p className="text-center text-gray-500 text-lg">Your cart is empty.</p>
+        <p className="text-center text-gray-500 text-base sm:text-lg">
+          Your cart is empty.
+        </p>
       )}
-      <div className="flex items-center justify-between p-2">
-        <h2 className="text-2xl">Total Amount: $2200</h2>
-        <div className="flex gap-2">
+      <div className="flex flex-col sm:flex-row items-center justify-between p-2">
+        <h2 className="md:text-2xl font-semibold">Total Amount: $2200</h2>
+        <div className="flex gap-1 md:gap-2 flex-wrap">
           <RedirectToProductBtn />
-          <button className="p-4 bg-lime-600 rounded-lg hover:bg-lime-400 transition-colors text-white">
+          <button className="p-3 md:p-4 bg-lime-600 rounded-lg hover:bg-lime-400 transition-colors text-white">
             Checkout
           </button>
         </div>
