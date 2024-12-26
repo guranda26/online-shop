1b2b42581056601c953c1e87ea6516be1075e084
Guranda Lemonjava <109870250+guranda26@users.noreply.github.com>
2024-12-27 02:41:25 +0400

    fix cart styles

--- src/app/[locale]/(dashboard)/cart/page.tsx
+++ src/app/[locale]/(dashboard)/cart/page.tsx
@@ -1,3 +1,6 @@
+import RedirectToProductBtn from "../../../components/RedirectToProductPage";
+import { MdDelete } from "react-icons/md";
+
 interface CartItem {
   id: number;
   created_at: string;
@@ -15,69 +18,71 @@ interface Product {
 }
 
 const Page = async () => {
-  const response = await fetch('http://localhost:3000/api/cart', {
-    cache: 'no-store',
+  const response = await fetch("http://localhost:3000/api/cart", {
+    cache: "no-store",
   });
   const cart: CartItem[] = await response.json();
 
   return (
-    <div className='w-[60vw] p-6 mx-auto'>
-      <h1 className='text-2xl font-bold text-gray-800 mb-6'>My Cart</h1>
+    <div className="w-[70vw] p-6 mx-auto">
+      <h1 className="text-2xl font-bold text-gray-800 mb-6">My Cart</h1>
       {cart.length > 0 ? (
-        <div className='overflow-x-auto'>
-          <table className='min-w-full bg-white border border-gray-200 rounded-lg shadow-md'>
-            <thead className='bg-gray-100'>
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white border border-gray-200 rounded-lg shadow-md">
+            <thead className="bg-gray-100">
               <tr>
-                <th className='text-left px-6 py-3 text-sm font-medium text-gray-700'></th>
-                <th className='text-left px-6 py-3 text-sm font-medium text-gray-700'>
+                <th className="px-6 py-3 text-sm font-medium text-gray-700"></th>
+                <th className="px-6 py-3 text-sm font-medium text-gray-700">
                   Product Name
                 </th>
-                <th className='text-left px-6 py-3 text-sm font-medium text-gray-700'>
+                <th className="px-6 py-3 text-sm font-medium text-gray-700">
                   Quantity
                 </th>
-                <th className='text-left px-6 py-3 text-sm font-medium text-gray-700'>
+                <th className="px-6 py-3 text-sm font-medium text-gray-700">
                   Price per item
                 </th>
-                <th className='text-left px-6 py-3 text-sm font-medium text-gray-700'>
+                <th className="px-6 py-3 text-sm font-medium text-gray-700">
                   Total Price
                 </th>
-                <th className='text-left px-6 py-3 text-sm font-medium text-gray-700'></th>
+                <th className="px-6 py-3 text-sm font-medium text-gray-700"></th>
               </tr>
             </thead>
             <tbody>
               {cart.map((item) => (
-                <tr key={item.id} className='border-t'>
-                  <td className='px-6 py-4'>
+                <tr key={item.id} className="border-t">
+                  <td className="px-6 py-4">
                     <img
                       src={item.products.image_link}
                       alt={item.products.name}
-                      className='w-16 h-16 object-cover rounded-lg'
+                      className="min-w-[20px] max-w-[60px] rounded-lg"
                     />
                   </td>
-                  <td className='px-6 py-4 text-gray-700'>
+                  <td className="px-6 py-4 text-gray-700">
                     {item.products.name}
                   </td>
-                  <td className='px-6 py-4 text-gray-700'>
-                    <form className='flex gap-2'>
+                  <td className="px-6 py-4">
+                    <form className="flex gap-2 flex-wrap">
                       <input
-                        type='number'
-                        className='input w-[70px] border'
-                        // value={item.quantity}
-                        min='1'
+                        type="number"
+                        className="input w-[70px] border border-gray-300 rounded-md p-1"
+                        min="1"
+                        value={1}
                       />
-                      <button className='btn btn-primary' type='submit'>
+                      <button className="px-3 py-1 rounded-md bg-blue-500 text-white focus:outline-none hover:bg-blue-600 transition-colors">
                         Update
                       </button>
                     </form>
                   </td>
-                  <td className='px-6 py-4 text-gray-700 font-semibold '>
+                  <td className="px-3 py-4 text-gray-700 font-semibold text-center">
                     {item.products.price}
                   </td>
-                  <td className='px-6 py-4 text-gray-700 font-semibold whitespace-nowrap'>
+                  <td className="pl-3 py-4 text-gray-700 font-semibold text-center">
                     {item.products.price}
                   </td>
-                  <td className='px-6 py-4'>
-                    <button className='btn btn-error'>Delete</button>
+                  <td className="px-1 py-4">
+                    <button className="py-1  text-red-600 transition-colors">
+                      <MdDelete size={20} />
+                    </button>
                   </td>
                 </tr>
               ))}
@@ -85,16 +90,13 @@ const Page = async () => {
           </table>
         </div>
       ) : (
-        <p className='text-center text-gray-500 text-lg'>Your cart is empty.</p>
+        <p className="text-center text-gray-500 text-lg">Your cart is empty.</p>
       )}
-      <div className='flex items-center justify-between p-2'>
-        <h2 className='text-2xl'>Total Amount: $2200</h2>{' '}
-        {/* Replace with actual total */}
-        <div className='flex gap-2'>
-          <button className='p-4 bg-teal-600 rounded-lg hover:bg-teal-400 transition-colors'>
-            Continue Shopping
-          </button>
-          <button className='p-4 bg-lime-600 rounded-lg hover:bg-lime-400 transition-colors'>
+      <div className="flex items-center justify-between p-2">
+        <h2 className="text-2xl">Total Amount: $2200</h2>
+        <div className="flex gap-2">
+          <RedirectToProductBtn />
+          <button className="p-4 bg-lime-600 rounded-lg hover:bg-lime-400 transition-colors text-white">
             Checkout
           </button>
         </div>
